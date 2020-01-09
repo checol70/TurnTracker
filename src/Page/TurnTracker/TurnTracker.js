1af17fe9f8c07e0c69c9e752001943629ce8cd6e
Sheckerol <checol70@gmail.com>
2020-01-09 00:45:48 -0700

    Fixed small issues

--- src/Page/TurnTracker/TurnTracker.js
+++ src/Page/TurnTracker/TurnTracker.js
@@ -14,7 +14,7 @@ class TurnTracker extends Component {
   state = {
     characters: [],
     initiative: [],
-    count: 1,
+    count: 10,
     name: "bill",
     modifier: 3,
     numRolled: 9,
@@ -31,7 +31,7 @@ class TurnTracker extends Component {
   };
 
   sortInitiative = initiative => {
-    const matcher = /.*[\d*]/i;
+    const matcher = /(\d\w*)/i;
     initiative.sort((a, b) => {
       let diff = a.count - b.count;
       // console.log(diff +"first")
@@ -50,7 +50,7 @@ class TurnTracker extends Component {
         a.count === b.count
       ) {
         diff = parseInt(matcher.exec(a.name)) - parseInt(matcher.exec(b.name));
-        // console.log(diff +"fourth")
+        console.log(matcher.exec(a.name))
       }
       if (diff > 0) {
         return 1;
@@ -58,6 +58,9 @@ class TurnTracker extends Component {
         return -1;
       } else return 0;
     });
+    let state = this.state;
+    state.initiative = initiative
+    this.setState(initiative);
     return initiative;
   };
 
@@ -71,29 +74,33 @@ class TurnTracker extends Component {
   }
 
   addInitiative = (numRolled, modifier, name) => {
+    // const matcher = /.*(\d*)/i;
     let arr = this.state.initiative;
     let numTurns =
       this.state.initiative.count > 0
         ? Math.min.apply(this.state.initiative.map(e => e.count))
         : 0;
-    arr.push({
-      numRolled: numRolled,
-      modifier: modifier,
-      name: name,
-      count: numTurns,
-      totalDamage:0,
-      damageInputValue:0
-    });
-    arr = this.sortInitiative(arr);
-    this.setState({
-      initiative: arr
-    });
+    // while(this.state.initiative.map(e=>e.name).includes(name)){
+    //    if(!parseInt(matcher.exec(name)).isNaN()){
+    //     // name.splice()
+    //    }
+    // }
+      arr.push({
+        numRolled: numRolled,
+        modifier: modifier,
+        name: name,
+        count: numTurns,
+        totalDamage:0,
+        damageInputValue:0
+      });
+      this.sortInitiative(arr);
   };
 
   cycleTurn = index => {
     let state = this.state;
     state.initiative[index].count++;
     state.checkerboard = state.checkerboard.reverse();
+    this.sortInitiative(state.initiative)
     this.setState(state);
   };
 
@@ -105,8 +112,7 @@ class TurnTracker extends Component {
 
   showInitiative = () => {
     let arr = [];
-    let sortedArr = this.sortInitiative(this.state.initiative);
-    sortedArr.forEach((element, index) => {
+    this.state.initiative.forEach((element, index) => {
       arr.push(
         <TurnPiece
           key={index}
@@ -122,7 +128,7 @@ class TurnTracker extends Component {
           damage = {()=>{console.log(index);console.log(element.totalDamage);console.log("Hurt!")}}
           heal = {()=>{console.log(element.name);console.log(element.totalDamage);console.log("Heal!") }}
           damageTaken = {element.totalDamage}
-          numValidate = {(event)=>{this.initiativeValidate(event.value,event.target)}}
+          numValidate = {(event)=>{this.initiativeValidate(event.target.value,event.target)}}
           value = {element.totalDamage}
         />
       );
@@ -135,16 +141,19 @@ class TurnTracker extends Component {
       this.addInitiative(
         numRolled,
         this.state.modifier,
-        i > 0 ? this.state.name + i : this.state.name
-      );
+        `${this.state.name}${i+1}`      
+        );
     }
     let state = this.state;
     state.modifier = 0;
     state.name = "";
     this.setState(state);
   };
-  initiativeValidate = event =>{
-    console.log(event.value)
+  initiativeValidate = (value, key) =>{
+    if(parseInt(value).isNaN()){
+      return;
+    }
+
   }
   numValidate = event => {
     if (event.target.value === "") {
